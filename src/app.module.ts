575915a04aa2b8637831545085aee26bbed2f249
Wseop <crazyz2000@naver.com>
2023-04-30 14:45:56 +0900

    feat:add package @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/loamanager'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URI),
     AdminModule,
   ],
 })
