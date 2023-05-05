b8adbd9d71defb533bf08756e289b16f9a870fac
Wseop <crazyz2000@naver.com>
2023-05-05 22:07:50 +0900

    remove:apikey test code

--- src/apikey/__test__/mocks/apikey.mock.ts
+++
@@ -1,9 +0,0 @@
-import { ApiKey } from 'src/apikey/schemas/apikey.schema';
-
-export class MockApiKeyModel {
-  datas = [{ apiKey: 'key1' }, { apiKey: 'key2' }, { apiKey: 'key3' }];
-
-  find() {
-    return this.datas;
-  }
-}
