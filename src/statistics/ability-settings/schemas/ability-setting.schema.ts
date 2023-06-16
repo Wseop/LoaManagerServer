af37497f6f776285a448892d14eecd8388e115f2
Wseop <crazyz2000@naver.com>
2023-06-16 19:05:36 +0900

    refactor:statistics-module & swagger

--- src/statistics/ability-settings/schemas/ability-setting.schema.ts
+++ src/statistics/ability-settings/schemas/ability-setting.schema.ts
@@ -1,26 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 @Schema()
 export class AbilitySetting {
   @Prop()
-  @IsNotEmpty()
-  @IsString()
   characterName: string;
 
   @Prop()
-  @IsNotEmpty()
-  @IsString()
   className: string;
 
   @Prop()
-  @IsNotEmpty()
-  @IsString()
   classEngrave: string;
 
   @Prop()
-  @IsNotEmpty()
-  @IsString()
   ability: string;
 }
 
--- src/statistics/engrave-settings/schemas/engrave-setting.schema.ts
+++ src/statistics/engrave-settings/schemas/engrave-setting.schema.ts
@@ -1,34 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Type } from 'class-transformer';
-import {
-  ArrayNotEmpty,
-  IsNotEmpty,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
 import { CharacterEngrave } from '../../../lostark/characters/interfaces/character-engrave.interface';
 
 @Schema()
 export class EngraveSetting {
   @Prop()
-  @IsNotEmpty()
-  @IsString()
   characterName: string;
 
   @Prop()
-  @IsNotEmpty()
-  @IsString()
   className: string;
 
   @Prop()
-  @IsNotEmpty()
-  @IsString()
   classEngrave: string;
 
   @Prop()
-  @ArrayNotEmpty()
-  @ValidateNested({ each: true })
-  @Type(() => CharacterEngrave)
   engraves: CharacterEngrave[];
 }
 
