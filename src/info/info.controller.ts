5c268e363617e4261fff385ddc980d30acfc130e
Wseop <crazyz2000@naver.com>
2023-06-16 14:05:11 +0900

    refactor info module & swagger

--- src/info/info.controller.ts
+++ src/info/info.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { InfoService } from './info.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { InfoDto } from './dto/info.dto';
 
 @ApiTags('Info')
 @Controller('info')
@@ -8,13 +9,13 @@ export class InfoController {
   constructor(private readonly infoService: InfoService) {}
 
   @Get()
-  @ApiOkResponse()
+  @ApiOkResponse({ type: [InfoDto] })
   findAll() {
     return this.infoService.findAll();
   }
 
   @Get(':key')
-  @ApiOkResponse()
+  @ApiOkResponse({ type: InfoDto })
   find(@Param('key') key: string) {
     return this.infoService.find(key);
   }
